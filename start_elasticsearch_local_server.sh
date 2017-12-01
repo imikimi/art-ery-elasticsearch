f3e466b60c27946ea8064ae3085091213cb091ce
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2017-12-01 11:27:20 -0800

    IMPROVED: start_elasticsearch_local_server.sh now deamonizes, creates a PID file, and on next run, kills the old server

--- start_elasticsearch_local_server.sh
+++ start_elasticsearch_local_server.sh
@@ -1,9 +1,35 @@
 #!/usr/bin/env bash
 VERSION=5.6.2
-cd `dirname "$0"`/devserver
-echo "ELASTICSEARCH: deleting old data"
-rm -rf elasticsearch-$VERSION/
+DIR=`pwd`/`dirname "$0"`/devserver
+PIDFILE=$DIR/elasticsearch.pid
+INSTALLDIR=$DIR/elasticsearch-$VERSION/
+echo DIR $DIR
+echo PIDFILE $PIDFILE
+echo INSTALLDIR $INSTALLDIR
+
+if [ -f "$PIDFILE" ]
+then
+  echo pwd `pwd`
+  PID=`cat $PIDFILE`
+  echo "ELASTICSEARCH: stopping old server"
+  echo "ELASTICSEARCH: kill $PID"
+  kill $PID
+  sleep .5
+  echo "ELASTICSEARCH: any old servers still running?"
+  ps aux | grep -i "java.*[e]lasticsearch"
+  echo ""
+fi
+
+echo "ELASTICSEARCH: deleting old data: $INSTALLDIR"
+rm -rf $INSTALLDIR
 echo "ELASTICSEARCH: unzipping v$VERSION"
-unzip elasticsearch-$VERSION.zip > /dev/null
-echo "ELASTICSEARCH: starting server v$VERSION"
-./elasticsearch-$VERSION/bin/elasticsearch
+unzip $DIR/elasticsearch-$VERSION.zip -d $DIR> /dev/null
+echo "ELASTICSEARCH: starting server v$VERSION with PIDFILE $PIDFILE"
+$INSTALLDIR/bin/elasticsearch -d -p $PIDFILE
+
+while [ ! -f "$PIDFILE" ]
+do
+  sleep 1
+  echo "ELASTICSEARCH: waiting for server to start..."
+done
+echo "ELASTICSEARCH: ready!"
